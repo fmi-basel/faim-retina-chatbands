a6aff4692f00877bad50b2964a7e4d16715c6c14
Fiona Muellner <71281970+fionamuellner@users.noreply.github.com>
2024-05-27 14:30:23 +0200

    Create segment_data.py

---
+++ segment_data.py
@@ -0,0 +1,6 @@
+# Before running, adjust folder names (/datafolder), segmentation task (retchat.task.*) and model (models\*)
+
+for DATA in "/datafolder" ; do
+	rm "/home/fiona.muellner/faim-retina-chatbands/outputs/stacks_to_process.txt"
+	luigi --local-scheduler --module retchat.tasks.run_segmentation_d20_upper99_5_d20 retina.ParallelChatbandPredictionTask --output-folder 'outputs' --input-folder "$DATA" --model-dir models/N500-percentile_max-block-20_231027/segm/unet-L4-D1-None/run000 --model-weights-fname model_best.h5
+done
