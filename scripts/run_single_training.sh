de589055139d396ce0312cb4cbb8f0f9b526c1e9
Fiona Muellner <71281970+fionamuellner@users.noreply.github.com>
2024-05-27 14:04:55 +0200

    Update run_single_training.sh
    Updated comment

--- scripts/run_single_training.sh
+++ scripts/run_single_training.sh
@@ -4,9 +4,9 @@ DSET=data/N500-percentile_max-block-20/
 # To train previous model with new data:
 python train_segmentation_frompretrained.py --batch_size=1 --lr_min=1e-7 --lr_max=1e-4 --dataset_folder ./${DSET} --epochs 50 --restarts 1 --outdir models-replicate/ --n_levels 4 --downsampling 1 --width 1.0
 
-# To train previous model with new data using low patience:
-# python train_segmentation_lowpatience.py --batch_size=1 --lr_min=1e-7 --lr_max=1e-4 --dataset_folder ./${DSET} --epochs 50 --restarts 1 --outdir models-replicate/ --n_levels 4 --downsampling 1 --width 1.0
-
 # To train from scratch (adjust downsampling):
 # python train_segmentation.py --batch_size=1 --lr_min=1e-7 --lr_max=1e-4 --dataset_folder ./${DSET} --epochs 500 --restarts 1 --outdir models-replicate/ --n_levels 4 --downsampling 2 --width 1.0
+
+# To train from scratch using low patience:
+# python train_segmentation_lowpatience.py --batch_size=1 --lr_min=1e-7 --lr_max=1e-4 --dataset_folder ./${DSET} --epochs 50 --restarts 1 --outdir models-replicate/ --n_levels 4 --downsampling 1 --width 1.0
 
