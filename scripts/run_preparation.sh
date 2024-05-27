076d3d5af7f86ff09519fa9669a269bb3501a273
Fiona Muellner <71281970+fionamuellner@users.noreply.github.com>
2024-05-27 13:37:40 +0200

    Update run_preparation.sh
    Updated to latest variables and generalized foldernames

--- scripts/run_preparation.sh
+++ scripts/run_preparation.sh
@@ -1,9 +1,11 @@
-NSAMPLES=200
+NSAMPLES=500
 OUTDIR=data/
-DELTA=10
+DELTA=20
 
 # NOTE we call the prepare script for each folder individually to make
 # sure that they are equally represented in the validation split.
-for DATA in "/data_fmi/mdrive/groska/Fiona/Confocal/190225 johnson" "/data_fmi/mdrive/groska/Fiona/Confocal/190320 Merian" ; do
-    python prepare_dataset.py --data_folder "$DATA" --label_folder "/tungstenfs/scratch/groska/Fiona/For Markus/" --samples_per_stack $NSAMPLES --output_folder $OUTDIR --delta $DELTA --projector percentile_max ;
+
+for DATA in "/datafolder"; do
+    python prepare_dataset.py --data_folder "$DATA" --label_folder "/targetfolder" --samples_per_stack $NSAMPLES --output_folder $OUTDIR --delta $DELTA --projector percentile_max;
 done
+
